a8b1623cd00d4cb8bda08e4eb393661a0b3ec799
Haluk Oksuzgonul <47000155+oksuzgonul@users.noreply.github.com>
2022-11-09 21:14:14 -0800

    converted all movement to rigidbody2d

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,6 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
    [SerializeField] private float speed;
-   [SerializeField] private float gravityConstant;
    [SerializeField] private float jumpPower;
    private PlayerInput _playerInput;
    private Rigidbody2D _rB;
@@ -13,12 +12,11 @@ public class PlayerController : MonoBehaviour
    private InputAction _moveAction;
    private InputAction _jumpAction;
    private Vector3 _moveByInput;
-   private bool _jumpInitiated;
    private const int MaxJump = 2;
    private int _jumpCount;
-   private Coroutine _endJumpCoroutine;
    private Coroutine _waitLandCoroutine;
 
+   public bool isPlayerGrounded;
    private void Awake()
    {
       _playerInput = GetComponent<PlayerInput>();
@@ -51,46 +49,37 @@ public class PlayerController : MonoBehaviour
    {
       StopAllCoroutines();
       _jumpCount = 0;
-      _jumpInitiated = false;
-      _moveByInput = Vector3.zero;
    }
    private void Jump(InputAction.CallbackContext context)
    {
-      if (_jumpInitiated) return;
       if (_jumpCount >= MaxJump)
       {
          if (_waitLandCoroutine != null) StopCoroutine(_waitLandCoroutine);
          _waitLandCoroutine = StartCoroutine(WaitUntilLanded());
          return;
       }
-      _jumpInitiated = true;
+      _rB.velocity += Vector2.up * jumpPower;
       _jumpCount += 1;
-      if (_endJumpCoroutine != null) StopCoroutine(_endJumpCoroutine);
-      _endJumpCoroutine = StartCoroutine(EndJump());
    }
    private IEnumerator WaitUntilLanded()
    {
       yield return new WaitUntil(IsPlayerGrounded);
       _jumpCount = 0;
-      _jumpInitiated = false;
-   }
-   private IEnumerator EndJump()
-   {
-      yield return new WaitForSeconds(0.5f);
-      _jumpInitiated = false;
-      StartCoroutine(WaitUntilLanded());
    }
    private void Move(InputAction.CallbackContext context)
    {
-      _moveByInput = new Vector3( context.ReadValue<Vector2>().x, 0, 0);
+      var xInput = context.ReadValue<Vector2>().x;
+      //less control on air
+      if (!IsPlayerGrounded()) xInput /= 2;
+      _moveByInput = new Vector3( xInput, 0, 0);
    }
    private void FixedUpdate()
    {
+      isPlayerGrounded = IsPlayerGrounded();
       _rB.velocity = new Vector2(_moveByInput.x * speed, _rB.velocity.y);
    }
-
    public bool IsPlayerGrounded()
    {
-      return _rB.velocity.y == 0;
+      return _rB.velocity.y < 0.1f;
    }
 }
